39cfdda449f14363a76dc4ec3845c3ee37746fe7
smk <sudhanshumohan781@gmail.com>
2021-10-09 20:24:14 +0530

    Basic setup done, need to work on map functionality

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-const express = require('express')
+//--------------------------------------------------------------------
+// Imports
+
+const express       = require('express')
+const bodyParser    = require('body-parser')
+const chalk         = require('chalk')
+const lusca         = require('lusca')
+
+
 const app = express()
 
+app.set("view engine","ejs")
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//--------------------------------------------------------------------
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/map', (req, res) => {
+  res.render('map.ejs')
+})
+
+app.get('*', (req, res) => {
+  res.render('404')
+})
+
+//--------------------------------------------------------------------
+// Server Parameter
+
 const hostname = '127.0.0.1'
 const port = 3000
 app.listen(port,hostname, () => {
-  console.log(`Example app listening at http://${hostname}:${port}`)
+  console.log(`Server started at http://${hostname}:${port}`)
+  console.log('  Press CTRL+c to stop!')
 })
